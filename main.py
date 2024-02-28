0a4c16e69f0d021a8874b38d5dcdfcca816a34ba
its-kenneth <88260663+its-kenneth@users.noreply.github.com>
2024-02-28 01:55:37 +0000

    final submission

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ seconds = int(seconds)
 
 # ... complete the code below
 
-
+minutes, seconds = divmod(seconds,60)
+hours, minutes = divmod(minutes,60)
 
 # Follow the formatting given
 # e.g. The duration is X hours, X minutes, and X seconds.
